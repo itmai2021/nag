c7a93113dbc44d698773351c2440bd7496b26b33
Deni Ardiyansyah <ardiyansyahdeni650@gmail.com>
2025-09-17 15:45:10 +0700

    gedung

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     .banner {
         height: 100vh;
         width: 100vw;
-        background-image: url("{{ asset('/assets/images/gedungMAI.png') }}");
+        background-image: url("{{ asset('/assets/images/gedungMAIFix.png') }}");
         background-size: 100% 100%;
         /* isi penuh tanpa ada ruang putih */
         background-repeat: no-repeat;
