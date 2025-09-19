ffdcc6a953b40d4f4daa0dfd78e4989cb423cf4d
Deni Ardiyansyah <ardiyansyahdeni650@gmail.com>
2025-09-19 14:01:13 +0700

    final

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     .banner {
         height: 100vh;
         width: 100vw;
-        background-image: url("{{ asset('/assets/images/gedungMAISangatFix.jpg') }}");
+        background-image: url("{{ asset('/assets/images/GedungMAIFinal.jpg') }}");
         background-size: 100% 100%;
         /* isi penuh tanpa ada ruang putih */
         background-repeat: no-repeat;
